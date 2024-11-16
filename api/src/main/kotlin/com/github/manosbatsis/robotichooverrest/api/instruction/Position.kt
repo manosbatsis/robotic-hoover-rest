d44c001dff2035c3cc2a4c1f08cf0c36a80821b0
manosbatsis <manosbatsis@gmail.com>
2024-11-16 12:43:17 +0200

    chore: licence edit

--- api/src/main/kotlin/com/github/manosbatsis/robotichooverrest/api/instruction/Position.kt
+++ api/src/main/kotlin/com/github/manosbatsis/robotichooverrest/api/instruction/Position.kt
@@ -1,7 +1,7 @@
 /*
  * Copyright (C) 2024 Manos Batsis
 
- * Foobar is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public
+ * This is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public
  * License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later
  * version.
  *
